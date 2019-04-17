d8085ea0f7ecafcd8890a6239d093f44c90ab5a6
Jason Less <jaless1997@gmail.com>
2019-04-16 21:21:55 -0700

    Renaming homework directories and descriptions

---
+++ H5-Rock-on/solution/words.hpp
@@ -0,0 +1,14 @@
+//
+//  words.hpp
+//  Project 5 Solution
+//
+//  Created by Jason Less on 11/19/15.
+//  Copyright © 2015 Jason Less. All rights reserved.
+//
+
+#ifndef words_hpp
+#define words_hpp
+
+#include <stdio.h>
+
+#endif /* words_hpp */
